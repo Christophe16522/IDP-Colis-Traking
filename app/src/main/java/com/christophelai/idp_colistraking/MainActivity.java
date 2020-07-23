d1958bc3c305c3dc9be6877315f50cf219bcd342
Christophe Lai <lai_christophe@hotmail.com>
2020-07-23 10:58:35 +0400

    Add Interface Scan code bare and take picture

--- app/src/main/java/com/christophelai/idp_colistraking/MainActivity.java
+++ app/src/main/java/com/christophelai/idp_colistraking/MainActivity.java
@@ -2,13 +2,36 @@ package com.christophelai.idp_colistraking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnTakePicture, btnScanBarecode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initViews() {
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        btnScanBarecode = findViewById(R.id.btnScanBarcode);
+        btnTakePicture.setOnClickListener(this);
+        btnScanBarecode.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnTakePicture:
+                startActivity(new Intent(MainActivity.this, PictureBarcodeActivity.class));
+                break;
+            case R.id.btnScanBarcode:
+                startActivity(new Intent(MainActivity.this,ScannedBarcodeActivity.class));
+        }
     }
 }
---
+++ app/src/main/java/com/christophelai/idp_colistraking/ScannedBarcodeActivity.java
@@ -0,0 +1,14 @@
+package com.christophelai.idp_colistraking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScannedBarcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanned_barcode);
+    }
+}
