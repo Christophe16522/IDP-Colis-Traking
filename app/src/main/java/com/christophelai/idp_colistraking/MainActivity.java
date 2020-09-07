55047dd111aab5fb9eeaabd934dcd48968828f89
Christophe Lai <lai_christophe@hotmail.com>
2020-09-07 12:25:28 +0400

    btn log out

--- app/src/main/java/com/christophelai/idp_colistraking/MainActivity.java
+++ app/src/main/java/com/christophelai/idp_colistraking/MainActivity.java
@@ -1,6 +1,8 @@
 package com.christophelai.idp_colistraking;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,7 +12,7 @@ import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    Button btnScanBarecode;
+    Button btnScanBarecode, btnLogout;
     SharedPreferences prf;
 
     @Override
@@ -26,8 +28,44 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private void initViews() {
         btnScanBarecode = findViewById(R.id.btnOpenScan);
+        btnLogout = findViewById(R.id.btnLogout);
         btnScanBarecode.setOnClickListener(this);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                builder1.setMessage("Voulez-vous vraiment vous déconnecter ?");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "oui",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Constant.SaveLog("{\n" +
+                                        "    \"btn\":\"yes - Log out\",\n" +
+                                        "    \"msg\":\"Log Out\"\n" +
+                                        "}", prf.getString("idCarrier", null), MainActivity.this);
+                                SharedPreferences.Editor editor = prf.edit();
+                                editor.clear();
+                                editor.commit();
+                                startActivity(new Intent(MainActivity.this, Login.class));
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "non",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
     }
+
 
     @Override
     public void onClick(View view) {
@@ -39,6 +77,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
                         "    \"btn\":\"btnOpenScan\",\n" +
                         "    \"msg\":\"Open ScannedBarcodeActivity\"\n" +
                         "}", prf.getString("idCarrier", null), MainActivity.this);
+
+            case R.id.btnSettings:
+                Log.w("Tag On Click", "btn Setting");
+                startActivity(new Intent(MainActivity.this, Settings.class));
+                Constant.SaveLog("{\n" +
+                        "    \"btn\":\"btnSettings\",\n" +
+                        "    \"msg\":\"Open Setting\"\n" +
+                        "}", prf.getString("idCarrier", null), MainActivity.this);
         }
     }
+
+
 }
