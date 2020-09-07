44e237a470578e27728d2b76552a7b5f644340d4
Christophe Lai <lai_christophe@hotmail.com>
2020-09-07 09:29:19 +0400

    add menu parameters and logout in main

---
+++ app/src/main/java/com/christophelai/idp_colistraking/Settings.java
@@ -0,0 +1,15 @@
+package com.christophelai.idp_colistraking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
