34f9f75dba0d3609d1df12128899deb51f452714
Christophe Lai <lai_christophe@hotmail.com>
2020-09-22 15:31:00 +0400

    send msg

--- app/src/main/java/com/christophelai/idp_colistraking/AppointmentBySMS.java
+++ app/src/main/java/com/christophelai/idp_colistraking/AppointmentBySMS.java
@@ -1,25 +1,20 @@
 package com.christophelai.idp_colistraking;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 public class AppointmentBySMS extends Activity {
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
     Button sendBtn;
     EditText txtphoneNo;
     EditText txtMessage;
-    String phoneNo;
-    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,46 +27,27 @@ public class AppointmentBySMS extends Activity {
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                sendSMSMessage();
+                sendSMS();
             }
         });
 
     }
 
-    protected void sendSMSMessage() {
-        phoneNo = txtphoneNo.getText().toString();
-        message = txtMessage.getText().toString();
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.SEND_SMS)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.SEND_SMS},
-                        MY_PERMISSIONS_REQUEST_SEND_SMS);
-            }
+    protected void sendSMS() {
+        Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+        smsIntent.setData(Uri.parse("smsto:"));
+        smsIntent.setType("vnd.android-dir/mms-sms");
+        smsIntent.putExtra("address", new String(txtphoneNo.getText().toString()));
+        smsIntent.putExtra("sms_body", txtMessage.getText().toString());
+        try {
+            startActivity(smsIntent);
+            finish();
+            Log.i("Finished sending SMS...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(AppointmentBySMS.this,
+                    "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
-                    Toast.makeText(getApplicationContext(), "SMS sent.",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-            }
-        }
 
-    }
 }
