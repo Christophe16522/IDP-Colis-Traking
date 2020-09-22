18662095ca599dc6e53f59dcf7b126fea5194398
Christophe Lai <lai_christophe@hotmail.com>
2020-09-22 18:03:28 +0400

    back up 220920

--- app/src/main/java/com/christophelai/idp_colistraking/AppointmentByMail.java
+++ app/src/main/java/com/christophelai/idp_colistraking/AppointmentByMail.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AppointmentByMail extends Activity {
-
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_by_mail);
+
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+        txtphoneNo = (EditText) findViewById(R.id.telephonNo);
+        txtMessage = (EditText) findViewById(R.id.txtMsg);
+
     }
 }
