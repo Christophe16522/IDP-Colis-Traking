e842201fab33fcf8046a2465f2d9ec89c0b05b78
Christophe Lai <lai_christophe@hotmail.com>
2021-03-09 10:12:29 +0400

    Ajout Saisi donne

--- app/src/main/java/com/christophelai/idp_colistraking/SaisieData.java
+++ app/src/main/java/com/christophelai/idp_colistraking/SaisieData.java
@@ -1,14 +1,12 @@
 package com.christophelai.idp_colistraking;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -27,61 +25,41 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Login extends Activity implements View.OnClickListener {
-    Button btnLogin;
-    ProgressBar progressBar;
-    String email, pass, url, res, status;
-    SharedPreferences pref;
-    EditText editEmail, editPass;
-
+public class SaisieData extends Activity implements View.OnClickListener {
+    Button btnCheckData;
+    String baseUrl = Constant.SERVER + "/";
+    EditText idComande;
+    TextView txtDelivery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        progressBar = findViewById(R.id.progressLogin);
-        editEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
-        editPass = (EditText) findViewById(R.id.editTextTextPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(this);
-        pref = getSharedPreferences("user_details", MODE_PRIVATE);
+        setContentView(R.layout.activity_saisie_data);
+        txtDelivery = findViewById(R.id.txtDelivery);
+
+        idComande = (EditText) findViewById(R.id.txtidCommande);
+        btnCheckData = findViewById(R.id.btnCheckData);
+        btnCheckData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("Tag On Click", "confirmer saisi des data");
+                getDeliveryById(idComande.getText().toString());
+                //startActivity(new Intent(MainActivity.this, SaisieData.class));
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnLogin:
-                progressBar.setVisibility(View.VISIBLE);
-                email = editEmail.getText().toString();
-                pass = editPass.getText().toString();
-                url = Constant.SERVER + "/api-login/" + email + "/" + pass;
-                login();
-        }
-    }
+    public void getDeliveryById(String nSuivi) {
 
-    public void login() {
+        String urlgetDeliveryById = baseUrl + "api-delivery/getDeliveryBynSuivi/" + nSuivi;
+        Log.i("getDeliveryById", "url : " + urlgetDeliveryById + " args : " + nSuivi);
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         try {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, (String) null, new Response.Listener<JSONObject>() {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, urlgetDeliveryById, (String) null, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
                     try {
-                        status = response.getString("status");
-                        res = response.getString("response");
-                        Log.i("Login", " args : " + res);
-
-                        if (status.equals("200")) {
-                            JSONObject user = new JSONObject(res);
-                            SharedPreferences.Editor editor = pref.edit();
-                            editor.putString("idCarrier", user.getString("id"));
-                            editor.putString("fullname", user.getString("fullname"));
-                            editor.putString("email", user.getString("email"));
-                            editor.commit();
-                            startActivity(new Intent(Login.this, MainActivity.class));
-                        } else if (status.equals("201")) {
-                            Toast.makeText(getApplicationContext(), res, Toast.LENGTH_LONG).show();
-                        }
-                        progressBar.setVisibility(View.GONE);
+                        txtDelivery.setText(response.getString("response"));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -105,12 +83,17 @@ public class Login extends Activity implements View.OnClickListener {
                         message = "Connection TimeOut! Please check your internet connection.";
                     }
                     Toast.makeText(getApplicationContext(), "Error : " + message, Toast.LENGTH_LONG).show();
-                    Log.e("erreur ssl : ",volleyError.getMessage());
                 }
             });
             requestQueue.add(jsonObjectRequest);
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Error : " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
